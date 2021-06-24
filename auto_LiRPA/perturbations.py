3f297ee1fce06d07532097f60bea268e98565675
Leomaru <33780483+Mahoumaru@users.noreply.github.com>
2021-06-24 18:41:48 +0900

    Support for Mahalanobis norm (i.e. multidim eps)

--- auto_LiRPA/perturbations.py
+++ auto_LiRPA/perturbations.py
@@ -7,13 +7,15 @@ import torch.nn as nn
 from auto_LiRPA.utils import logger, eyeC, LinearBound, Patches, BoundList
 import torch.nn.functional as F
 
+from numbers import Number
+
 class Perturbation:
     def __init__(self):
         pass
 
     def set_eps(self, eps):
         self.eps = eps
-    
+
     def concretize(self, x, A, sign=-1, aux=None):
         raise NotImplementedError
 
@@ -31,7 +33,7 @@ class PerturbationL0Norm(Perturbation):
     def concretize(self, x, A, sign = -1, aux = None):
         if A is None:
             return None
-        
+
         eps = math.ceil(self.eps)
         x = x.reshape(x.shape[0], -1, 1)
         center = A.matmul(x)
@@ -69,18 +71,26 @@ class PerturbationL0Norm(Perturbation):
         eye = torch.eye(dim).to(x.device).unsqueeze(0).repeat(batch_size, 1, 1)
         lw = eye.reshape(batch_size, dim, *x.shape[1:])
         lb = torch.zeros_like(x).to(x.device)
-        uw, ub = lw.clone(), lb.clone()       
+        uw, ub = lw.clone(), lb.clone()
         return LinearBound(lw, lb, uw, ub, x_L, x_U), x, None
 
-    
+
     def __repr__(self):
         return 'PerturbationLpNorm(norm=0, eps={})'.format(self.eps)
 
 """Perturbation constrained by the L_p norm."""
 class PerturbationLpNorm(Perturbation):
     def __init__(self, eps, norm=np.inf, x_L=None, x_U=None):
-        self.eps = eps
-        self.norm = norm
+        if not isinstance(eps, Number):
+            if not isinstance(eps, torch.Tensor):
+                self.eps = torch.tensor(eps)
+                if len(self.eps.shape) == 1:
+                    self.eps = torch.diag(self.eps)
+            assert self.eps.shape[0] == self.eps.shape[1], "Argument [eps] must form a n by n square matrix."
+            self.norm = 2
+        else:
+            self.eps = eps
+            self.norm = norm
         self.dual_norm = 1 if (norm == np.inf) else (np.float64(1.0) / (1 - 1.0 / self.norm))
         self.x_L = x_L
         self.x_U = x_U
@@ -112,11 +122,17 @@ class PerturbationLpNorm(Perturbation):
                 x = x.reshape(x.shape[0], -1, 1)
                 if not isinstance(A, eyeC):
                     # Find the upper and lower bounds via dual norm.
-                    deviation = A.norm(self.dual_norm, -1) * self.eps
+                    if isinstance(self.eps, Number):
+                        deviation = A.norm(self.dual_norm, -1) * self.eps
+                    else:
+                        deviation = A.matmul(self.eps.transpose(0, 1)).norm(self.dual_norm, -1)
                     bound = A.matmul(x) + sign * deviation.unsqueeze(-1)
                 else:
                     # A is an identity matrix. Its norm is all 1.
-                    bound = x + sign * self.eps
+                    if isinstance(self.eps, Number):
+                        bound = x + sign * self.eps
+                    else:
+                        bound = x + sign * self.eps.transpose(0, 1).norm(self.dual_norm, -1)
             bound = bound.squeeze(-1)
             return bound
 
@@ -140,7 +156,7 @@ class PerturbationLpNorm(Perturbation):
                     prod = prod.sum((-1, -2, -3)).transpose(-2, -1)
                     # size of prod: [batch_size, out_c, L], and then reshape it to [batch_size, out_c, M, M]
                     bound = prod.view(prod.size(0), prod.size(1), int(math.sqrt(prod.size(2))), int(math.sqrt(prod.size(2))))
- 
+
                     unfold_input = F.unfold(diff, kernel_size=A.patches.size(-1), padding = A.padding, stride = A.stride).transpose(-2, -1)
                     unfold_input = unfold_input.view(unfold_input.size(0), unfold_input.size(1), -1, A.patches.size(-3), A.patches.size(-2), A.patches.size(-1))
                     prod = unfold_input * A.patches.abs()
@@ -153,7 +169,7 @@ class PerturbationLpNorm(Perturbation):
             else:# Lp norm
                 x_L = x - self.eps if self.x_L is None else self.x_L
                 x_U = x + self.eps if self.x_U is None else self.x_U
-                
+
                 raise NotImplementedError()
 
         if isinstance(A, eyeC) or isinstance(A, torch.Tensor):
@@ -182,7 +198,7 @@ class PerturbationLpNorm(Perturbation):
         eye = torch.eye(dim).to(x.device).unsqueeze(0).repeat(batch_size, 1, 1)
         lw = eye.reshape(batch_size, dim, *x.shape[1:])
         lb = torch.zeros_like(x).to(x.device)
-        uw, ub = lw.clone(), lb.clone()       
+        uw, ub = lw.clone(), lb.clone()
         return LinearBound(lw, lb, uw, ub, x_L, x_U), x, None
 
     def __repr__(self):
@@ -229,7 +245,7 @@ class PerturbationSynonym(Perturbation):
         num_pos = int(np.max(np.sum(can_be_replaced, axis=-1)))
         update_A = A.shape[-1] > num_pos * dim_word
         if update_A:
-            bias = torch.bmm(A, (x * (1 - mask_rep).unsqueeze(-1)).reshape(batch_size, -1, 1)).squeeze(-1)        
+            bias = torch.bmm(A, (x * (1 - mask_rep).unsqueeze(-1)).reshape(batch_size, -1, 1)).squeeze(-1)
         else:
             bias = 0.
         A = A.reshape(batch_size, dim_out, -1, dim_word)
@@ -261,7 +277,7 @@ class PerturbationSynonym(Perturbation):
         mask = torch.cat(mask_new).reshape(batch_size, num_pos, max_num_cand)
         length = num_pos
 
-        A = A.reshape(batch_size, A.shape[1], length, -1).transpose(1, 2) 
+        A = A.reshape(batch_size, A.shape[1], length, -1).transpose(1, 2)
         x = x.reshape(batch_size, length, -1, 1)
 
         if sign == 1:
@@ -271,8 +287,8 @@ class PerturbationSynonym(Perturbation):
 
         init_tensor = torch.ones(batch_size, dim_out).to(x.device) * init
         dp = [[init_tensor] * (self.budget + 1) for i in range(0, length + 1)]
-        dp[0][0] = torch.zeros(batch_size, dim_out).to(x.device)     
- 
+        dp[0][0] = torch.zeros(batch_size, dim_out).to(x.device)
+
         A = A.reshape(batch_size * length, A.shape[2], A.shape[3])
         Ax = torch.bmm(
             A,
@@ -293,10 +309,10 @@ class PerturbationSynonym(Perturbation):
             dp[i][0] = dp[i - 1][0] + Ax[:, i - 1]
             for j in range(1, self.budget + 1):
                 dp[i][j] = cmp(
-                    dp[i - 1][j] + Ax[:, i - 1], 
+                    dp[i - 1][j] + Ax[:, i - 1],
                     dp[i - 1][j - 1] + Ax_rep_bound[:, i - 1]
                 )
-        dp = torch.cat(dp[length], dim=0).reshape(self.budget + 1, batch_size, dim_out)        
+        dp = torch.cat(dp[length], dim=0).reshape(self.budget + 1, batch_size, dim_out)
 
         return cmp(dp, dim=0).values + bias
 
@@ -332,8 +348,8 @@ class PerturbationSynonym(Perturbation):
         if forward:
             eye = torch.eye(dim_word).to(x.device)
             lw = torch.zeros(batch_size, dim, length, dim_word).to(x.device)
-            lb = torch.zeros_like(x).to(x.device)   
-        x_new = []     
+            lb = torch.zeros_like(x).to(x.device)
+        x_new = []
         word_embeddings = self.model.word_embeddings.weight
         vocab = self.model.vocab
         x_rep = [[[] for i in range(length)] for t in range(batch_size)]
@@ -342,8 +358,8 @@ class PerturbationSynonym(Perturbation):
             candidates = batch[t]['candidates']
             # for transformers
             if tokens[t][0] == '[CLS]':
-                candidates = [[]] + candidates + [[]]  
-            cnt = 0    
+                candidates = [[]] + candidates + [[]]
+            cnt = 0
             for i in range(length):
                 if can_be_replaced[t][i]:
                     word_embed = word_embeddings[vocab[tokens[t][i]]]
@@ -360,13 +376,13 @@ class PerturbationSynonym(Perturbation):
                     cnt += 1
                 else:
                     if forward:
-                        lb[t, i, :] = x[t, i, :]        
+                        lb[t, i, :] = x[t, i, :]
         if forward:
             uw, ub = lw, lb
         else:
             lw = lb = uw = ub = None
         zeros = torch.zeros(dim_word, device=x.device)
-        
+
         x_rep_, mask = [], []
         for t in range(batch_size):
             for i in range(length):
@@ -376,7 +392,7 @@ class PerturbationSynonym(Perturbation):
         mask = torch.tensor(mask, dtype=torch.float32, device=x.device)\
             .reshape(batch_size, length, max_num_cand)
         x_rep_ = x_rep_ * self.eps + x.unsqueeze(2) * (1 - self.eps)
-        
+
         inf = 1e20
         lower = torch.min(mask.unsqueeze(-1) * x_rep_ + (1 - mask).unsqueeze(-1) * inf, dim=2).values
         upper = torch.max(mask.unsqueeze(-1) * x_rep_ + (1 - mask).unsqueeze(-1) * (-inf), dim=2).values
